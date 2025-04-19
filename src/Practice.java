c46a11c380ac108547fe2c434d6a42be4a178a96
Kaustubh Raj <manman3852@gmail.com>
2025-04-19 22:50:40 +0530

    Initial commit

---
+++ src/Practice.java
@@ -0,0 +1,7 @@
+public class Practice{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("I am learning Java");
+        System.out.println("it is awesome");
+    }
+}
