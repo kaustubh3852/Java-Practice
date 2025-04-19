7494ec344c440e10ede9220968e0f6c5354a338e
Kaustubh Raj <manman3852@gmail.com>
2025-04-19 23:10:32 +0530

    Quotes needed

---
+++ src/Quotes.java
@@ -0,0 +1,6 @@
+public class Quotes{
+    public static void main(String[] args){
+//        System.out.println(This sentence will produce an error);
+        System.out.println("This sentence sill not produce an error");
+    }
+}
