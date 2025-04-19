d304524b4c8d0246772d78fc37dd755789b107e9
Kaustubh Raj <manman3852@gmail.com>
2025-04-19 23:46:03 +0530

    integer

---
+++ src/Practice8.java
@@ -0,0 +1,7 @@
+public class Practice8{
+    public static void main(String[] args){
+        final int myNum = 15;
+//        myNum = 20;   This will generate error;
+        System.out.println(myNum);
+    }
+}
