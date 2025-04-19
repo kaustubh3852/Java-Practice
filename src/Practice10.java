0bb8a353ce65e9dd0adf93c56f885ac7dac2f558
Kaustubh Raj <manman3852@gmail.com>
2025-04-19 23:53:13 +0530

    String

---
+++ src/Practice10.java
@@ -0,0 +1,8 @@
+public class Practice10 {
+    public static void main(String[] args){
+        String firstName = "Kaustubh ";
+        String lastName = "Sinha";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+    }
+}
