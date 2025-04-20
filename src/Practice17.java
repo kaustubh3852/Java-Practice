94c62fe03e7dbe3a82ab8d666d48d60c7d22c871
Kaustubh Raj <manman3852@gmail.com>
2025-04-20 10:20:16 +0530

    primitive

---
+++ src/Practice17.java
@@ -0,0 +1,16 @@
+public class Practice17 {
+    public static void main(String[] args){
+        byte myNum = 100;
+        short myNum2 = 5000;
+        int myNum3 = 100000;
+        long myNum4 = 10000000000000000l;
+        float myNum5 = 5.5555f;
+        double myNum6 = 5.55555d;
+        System.out.println(myNum);
+        System.out.println(myNum2);
+        System.out.println(myNum3);
+        System.out.println(myNum4);
+        System.out.println(myNum5);
+        System.out.println(myNum6);
+    }
+}
