239fe3f913415787ceae6fbf491bd2b0679c0712
Kaustubh Raj <manman3852@gmail.com>
2025-04-20 10:08:18 +0530

    area

---
+++ src/Project16.java
@@ -0,0 +1,15 @@
+public class Project16{
+    public static void main(String[] args){
+        // Create integer variable
+        int length = 4;
+        int width = 6;
+
+        // Calculate area of ractangle
+        int area = length * width;
+
+        // Print variables
+        System.out.println("Length is: " + length);
+        System.out.println("Width is: " + width);
+        System.out.println("Area of rectangle is: " + area);
+    }
+}
