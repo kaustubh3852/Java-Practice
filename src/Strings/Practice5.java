c2f141b44a3adbe7fa39de234bdc07ae5bf7b9d8
Kaustubh Raj <manman3852@gmail.com>
2025-04-20 17:08:16 +0530

    Strings & Numbers

---
+++ src/Strings/Practice5.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Practice5 {
+    public static void main(String[] args){
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+        System.out.println(z);
+    }
+}
