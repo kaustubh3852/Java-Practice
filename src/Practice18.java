c5174a06e88211f4459462c0ac70cb2555e562cd
Kaustubh Raj <manman3852@gmail.com>
2025-04-20 10:46:39 +0530

    different datatype

---
+++ src/Practice18.java
@@ -0,0 +1,15 @@
+public class Practice18 {
+    public static void main(String[] args){
+        //Create variables of different data types
+        int items = 50;
+        float costPerItem = 9.99f;
+        float totalCost = items * costPerItem;
+        char currency = '$';
+
+        //Print variables
+        System.out.println("Numbers of items: " + items);
+        System.out.println("Cost per item: " + costPerItem+currency);
+        System.out.println("Total cost: " + totalCost+currency);
+
+    }
+}
