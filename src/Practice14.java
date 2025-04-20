66dc375f0c4d93b75612fcd1eadc3410be40fc80
Kaustubh Raj <manman3852@gmail.com>
2025-04-20 09:51:01 +0530

    Identifiers

---
+++ src/Practice14.java
@@ -0,0 +1,12 @@
+public class Practice14 {
+    public static void main(String[] args){
+        // Good
+        int minutesPerHour = 60;
+
+        // It is ok but not easy to understand what m actually is
+        int m = 60;
+
+        System.out.println(minutesPerHour);
+        System.out.println(m);
+    }
+}
