7bb54060ee07489f554c9c7e26ec89084b1d95dc
Kaustubh Raj <manman3852@gmail.com>
2025-04-20 10:02:45 +0530

    Different data types

---
+++ src/Practice15.java
@@ -0,0 +1,18 @@
+public class Practice15 {
+    public static void main(String[] args){
+        // Student data
+        String studentName = "Anurag Tiwari";
+        int studentID = 15;
+        int studentAge = 24;
+        float studentFee = 75.25f;
+        char studentGrade = 'B';
+
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Student Age: " + studentAge);
+        System.out.println("Student Fee: " + studentFee);
+        System.out.println("Student Grade: " + studentGrade);
+
+
+    }
+}
