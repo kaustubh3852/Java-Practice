926e1315dfe84ff60bbfc93ebbeb8c4a61111c31
Kaustubh Raj <manman3852@gmail.com>
2025-05-09 20:23:35 +0530

    for-loop

---
+++ src/ifElse/Practice24.java
@@ -0,0 +1,9 @@
+package ifElse;
+
+public class Practice24 {
+    public static void main(String[] args){
+        for(int i = 0; i <= 10; i= i+2){
+            System.out.println(i);
+        }
+    }
+}
