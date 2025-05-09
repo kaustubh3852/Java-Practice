0e2837e0623453ec971d9b75d866610500b43871
Kaustubh Raj <manman3852@gmail.com>
2025-05-09 23:54:14 +0530

    2D Array

---
+++ src/Multidimentional/Practice1.java
@@ -0,0 +1,8 @@
+package Multidimentional;
+
+public class Practice1 {
+    public static void main(String[] args){
+        int [][] myNumbers = { {1,2,3,4}, {5,6,7}};
+        System.out.println(myNumbers[1][2]);
+    }
+}
