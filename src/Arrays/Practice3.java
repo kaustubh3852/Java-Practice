a9c87da32a5dec44d2e5ab24b000816f2245166b
Kaustubh Raj <manman3852@gmail.com>
2025-05-09 23:42:02 +0530

    loop

---
+++ src/Arrays/Practice3.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class Practice3 {
+    public static void main(String[] args){
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (int i = 0; i < cars.length; i++){
+            System.out.println(cars[i]);
+        }
+    }
+}
