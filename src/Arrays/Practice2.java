c1f652ddf49d715046e7ea023a9c99feb10e3062
Kaustubh Raj <manman3852@gmail.com>
2025-05-09 20:35:15 +0530

    Array

---
+++ src/Arrays/Practice2.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        cars[0] = "Opel";
+        System.out.println(cars[0]);
+
+    }
+}
