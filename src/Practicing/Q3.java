eee06063ea3cc465e18d300a483951921f7bb255
Kaustubh Raj <manman3852@gmail.com>
2025-05-10 13:08:18 +0530

    EvenOdd

---
+++ src/Practicing/Q3.java
@@ -0,0 +1,12 @@
+package Practicing;
+
+public class Q3 {
+    public static void main(String[] args){
+        int a = 10, b = 12;
+        if(a>b){
+            System.out.println(a + " is greater than "  + b);
+        }else {
+            System.out.println(b + " is greater than " + a);
+        }
+    }
+}
