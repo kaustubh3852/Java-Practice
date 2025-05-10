36a2e7b5d31e05db0b9a9d63f850d222ba421675
Kaustubh Raj <manman3852@gmail.com>
2025-05-10 13:04:11 +0530

    EvenOdd

---
+++ src/Practicing/Q2.java
@@ -0,0 +1,12 @@
+package Practicing;
+
+public class Q2 {
+    public static void main(String[] args){
+        int num = 10;
+        if(num % 2 == 0){
+            System.out.println("Even");
+        }else {
+            System.out.println("Odd");
+        }
+    }
+}
